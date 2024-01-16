bf79057772233c432784a536a74e8a0f842a2007
js809 <siegelmanjoseph@gmail.com>
2024-01-16 17:35:15 -0600

    update interface to draw lines and make cells. Centered window to screen

--- interface.py
+++ interface.py
@@ -1,11 +1,20 @@
-import tkinter as tk
+from tkinter import Tk, BOTH, Canvas
 
 
 class Window:
     def __init__(self, width, height):
-        self.__root = tk.Tk()
+        self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.__canvas = tk.Canvas(self.__root, height=height, width=width, bg="white")
+
+        # Calculate center coordinates
+        screen_width = self.__root.winfo_screenwidth()
+        screen_height = self.__root.winfo_screenheight()
+        x = (screen_width - width) // 2
+        y = (screen_height - height) // 2
+
+        # Set window geometry
+        self.__root.geometry(f"{width}x{height}+{x}+{y}")
+        self.__canvas = Canvas(self.__root, height=height, width=width, bg="white")
         self.__canvas.pack()
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
@@ -22,3 +31,55 @@ class Window:
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line, fill_color = "black"):
+        line.draw(self.__canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+        canvas.pack(fill=BOTH, expand=1)
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+
+    #def draw_move(self, to_cell, undo = False):
+
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from interface import Window
+from interface import Window, Line, Point, Cell
 
 def main():
     window = Window(800, 600)
-    window.wait_for_close()
+    c = Cell(window)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
 
+    c = Cell(window)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(window)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(window)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
+    window.wait_for_close()
 
 main()
